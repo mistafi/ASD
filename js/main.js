def4469303e8abc6b2bc1910bcc407f901b2d8a0
mistafi <mistafi@gmail.com>
2013-04-09 22:58:42 -0400

    adding additional assets

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+// ASD Project 1 
+// Joshua Wisecup
+// Term 1304
+
+$('#front-page').live('pagebeforeshow', function(){
+	alert("hello world");
+});	
+		
+$('#edit-page').live('pagebeforeshow', function(){
+
+});
+
+$('#display-page').bind('pageinit', function(event){
+
+});
+
+
+
+	
+
+
+
+
